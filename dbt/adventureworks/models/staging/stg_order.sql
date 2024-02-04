37f89d28e1c96ec0145572673538fbdc48abadd5
mohamedemad11 <mohamedemad353@gmail.com>
2024-02-04 16:54:07 +0200

    Fixed all the issues , now writing some analytical sql in fact_order is remainaing

--- dbt/adventureworks/models/staging/stg_order.sql
+++ dbt/adventureworks/models/staging/stg_order.sql
@@ -18,7 +18,7 @@ from {{source('adventureworks','SalesOrderDetail')}}
 source_3 as (
 
 select H.SalesOrderID , OrderDate , ShipDate  , CustomerID , SalesPersonID , TerritoryID , ShipMethodID , SubTotal , TaxAmt , Freight , TotalDue , SalesOrderDetailID ,
-ProductID , OrderQty , UnitPrice 
+ProductID , OrderQty , UnitPrice ,  row_number () over (partition by SalesOrderDetailID) as row_n , current_timestamp() as ingestion_timestamp 
 
 from source H
 left join source_2 D
@@ -27,5 +27,8 @@ on H.SalesOrderID = D.SalesOrderID
 
 )
 
-SELECT * , current_timestamp() as ingestion_timestamp
-FROM source_3
+
+SELECT * 
+except (row_n) , 
+from source_3
+where row_n = 1 
--- dbt/adventureworks/models/warehouse/fact_order.sql
+++ dbt/adventureworks/models/warehouse/fact_order.sql
@@ -8,11 +8,12 @@ with source as (
     , {{ dbt_utils.generate_surrogate_key(['TerritoryID']) }} as territory_key 
     , {{ dbt_utils.generate_surrogate_key(['OrderDate']) }} as order_date_key 
     , {{ dbt_utils.generate_surrogate_key(['ShipDate']) }} as ship_date_key 
-    ,
-    *
+    , 
+    * 
+
     from {{ref ('stg_order')}}
 )
 
 
-select *
+select * , 
 from source 
